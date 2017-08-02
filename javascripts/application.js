2af8d4e5c5cecff63fb7c2f2e2bf7b90828269a2
Philippe Decrat <philippe.decrat@gmail.com>
2017-08-02 17:52:24 +0200

    fix mail body

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 // Language choice
 	var language = "" ;
-
 	// si on clic sur un drapeau, on récupère la langue
 	$('#french').on('click' , function () {
 		language = "French";
@@ -16,6 +15,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -31,6 +31,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -46,6 +47,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -61,6 +63,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -76,6 +79,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -91,6 +95,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -106,6 +111,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -121,6 +127,7 @@ $(document).ready(function() {
 		$('#russian').addClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -136,6 +143,7 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').addClass('languechoisie');
 		$('#turkish').removeClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -151,11 +159,13 @@ $(document).ready(function() {
 		$('#russian').removeClass('languechoisie');
 		$('#portuguese').removeClass('languechoisie');
 		$('#turkish').addClass('languechoisie');
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
 	$('#other').on('click' , function () {
 		language = "your language";
+		sessionStorage.setItem('language', language)
 		console.log(language);
 	});
 
@@ -1539,7 +1549,6 @@ $(document).ready(function() {
 
 
 // resultats
-	var message;
 	$('#result').on('click' , function () {
 		var step = (readingGoal+writingGoal+speakingGoal+listeningGoal)-(readingLevel+writingLevel+speakingLevel+listeningLevel);
 		console.log("step" + step)
@@ -1566,11 +1575,12 @@ $(document).ready(function() {
 		console.log('nombre annee' + nombreAnnee)
 
 
-		message = jours[date.getDay()] + " ";   // nom du jour
+		var message = jours[date.getDay()] + " ";   // nom du jour
      	message += date.getDate() + "th ";   // numero du jour
      	message += mois[date.getMonth() + moisRestant] + " ";   // mois
      	message += date.getFullYear() + nombreAnnee;
      	console.log(message);
+		sessionStorage.setItem('message', message)
 		$('#fluencyDate').html(message);
 	})
 
@@ -1831,8 +1841,7 @@ $(document).ready(function() {
 		var elt2 = document.getElementById('friend-email');
 		var friendEmail = elt2.innerText || elt2.textContent;
 		console.log(friendEmail);
-		console.log(language);
-		console.log(message);
+ 		console.log(sessionStorage.getItem("language"));
 		var emailBody = 'I’ll reach fluency in Spanish on Monday 30th September 2017!%0D%0ADo you think you can be quicker?%0D%0AFind out for yourself with this quick test: www.how-long-to-become-fluent.com%0D%0AGood luck!!%0D%0Axxx';
 		console.log(emailBody);
 	})
